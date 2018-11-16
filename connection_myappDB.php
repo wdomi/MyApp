b917a321d62ca208b6788c1ed0bc6b172a520b45
Dominique Waldvogel <dominique.waldvogel@gmail.com>
2018-11-16 17:11:45 +0100

    trying to create table via php

--- connection_myappDB.php
+++ connection_myappDB.php
@@ -1,21 +1,35 @@
 <?php
 
-//°° ---------- connection to database and query as PHP-script ----------
+//+ ---------- Database connection, modification and query as PHP-script ----------
 
 //_ this is just so you are allowed to see the error print while developing, take it off when it goes for real!
 ini_set("display_errors", 1); 
 
-//? 1) CONNECTION: Execute connection to database server (create connection object)
+//! 1) CONNECTION: Execute connection to database server (create connection object)
 include('conf/config.php');
 
-//? 2) CREATE TABLE: create table
-        
+//! 2) CREATE TABLE: create table
 
-//* 3) QUERY: Prepare query (create query object)
+$table = "CREATE TABLE persons(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(70) NOT NULL UNIQUE
+)";
+
+// if (mysqli_query($conn, $table)) {
+//   echo "Table created successfully.";
+// } else {
+//   echo "ERROR: Could not able to execute $table. " . mysqli_error($conn);
+// }
+        
+/*
+
+//! 3) QUERY: Prepare query (create query object)
 $query = $conn->prepare('select sampleID, "col.iris", kind, age, sex from samples where sampleID like "AZ%" order by sampleID asc'); // query in SQL language
 
 
-//* 3) Execute statement (also object)
+//? 3) Execute statement (also object)
 $query->execute();
 
 //_ Check conection: gives the kind of error in case SQL or connection are not correct
@@ -27,17 +41,17 @@ if ($query->errorCode() > 0) {
 }
 
 
-//* 3a) GET & SHOW short: Get data (row by row)
+//? 3a) GET & SHOW short: Get data (row by row)
 // $row = $query->fetch();
 // Data gets taken from table and put in form to associative array by php
 
-//*  GET & SHOW short: Show data in a line
+//?  GET & SHOW short: Show data in a line
 // var_dump($row);
 
 // echo '<br><br><br>';
 
 
-//* 3b)  GET & SHOW extended: for better overview
+//? 3b)  GET & SHOW extended: for better overview
 while ($row = $query->fetch()) {			// This is an associative array, keys=fieldnames
   $sampleid = $row['sampleID'];
   $colony = $row['col.iris'];
@@ -51,16 +65,13 @@ while ($row = $query->fetch()) {			// This is an associative array, keys=fieldna
         $sex<br><br>";
 }
 
+
+ */
+
+
 ?>
 
 
 
-<!-- You need to have also this file to deny access to security folder "conf" and protect "config.php" containing the password and username
-
-Nested in:
-conf/.htaccess:
-
-Contains only one line of code:
-deny from all
-
--->
+<!-- You need to have also this file to deny access to security folder "conf" and protect "config.php" containing the
+    password and username Nested in : conf / . htaccess : Contains only one line of code : deny from all -->
