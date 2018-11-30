9be5ec647d3b24f64255630843168512fecabcd7
Dominique Waldvogel <dominique.waldvogel@gmail.com>
2018-11-30 15:52:23 +0100

    friday

---
+++ README.sh
@@ -0,0 +1,22 @@
+#? BACKUP VERSION HISTORY
+
+#° MyApp
+cd /Users/domiadmin/.bitnami/stackman/machines/xampp/volumes/root/htdocs/myapp
+# git pull https://github.com/wdomi/MyApp.git
+# git push --set-upstream https://github.com/wdomi/MyApp.git master
+
+#° config
+cd /Users/domiadmin/.bitnami/stackman/machines/xampp/volumes/root/etc
+# git pull https://github.com/wdomi/XAMPP_configFiles.git
+# git push --set-upstream https://github.com/wdomi/XAMPP_configFiles.git master 
+
+#° beispiele
+cd /Users/domiadmin/.bitnami/stackman/machines/xampp/volumes/root/htdocs/beispiele
+# git pull https://github.com/wdomi/Besipiel.git
+# git push --set-upstream https://github.com/wdomi/Besipiel.git master
+
+#° Pedigree
+cd /Users/domiadmin/.bitnami/stackman/machines/xampp/volumes/root/htdocs/pedigree
+# git pull https://github.com/wdomi/Pedigree.git
+# git push --set-upstream https://github.com/wdomi/Pedigree.git master
+
